a1edb76dcf9c21e1c0ba27110d09912d4c456493
Rithik Kumar Singh <rksbugatti@gmal.com>
2024-09-26 01:03:57 +0530

    final files

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const express =require("express");
-const http=require("http");
-const socketio=require("socket.io");
-const path=require("path");
+const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
@@ -11,12 +11,23 @@ const io = socketio(server);
 app.set("view engine", "ejs");
 
 // Use app.use to serve static files from the "public" directory
-app.set(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", function (socket) {
+    socket.on("send-location",(data)=>{
+        io.emit("receive-location",{id:socket.id,...data})
+    })
+
+    socket.on("disconnect",()=>{
+        io.emit("user-disconnected",socket.id);
+    })
+});
 
 app.get("/", (req, res) => {
-  res.send("Hello");
+    res.render("index");
+    // res.send("Hey");
 });
 
 server.listen(3000, () => {
-  console.log("Server is running on port 3000");
+    console.log("Server is running on port 3000");
 });
